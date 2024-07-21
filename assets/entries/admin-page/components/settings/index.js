83c1412b8465ac9d19df22cc69433bc154b0e1c5
Timothy Jacobs <timothy@ironbounddesigns.com>
2024-07-21 17:19:10 -0400

    Fix positioning of settings popover. Delete deprecated prop.

--- assets/entries/admin-page/components/settings/index.js
+++ assets/entries/admin-page/components/settings/index.js
@@ -14,28 +14,54 @@ import './style.css';
 
 function Settings() {
 	const [ open, setOpen ] = useState();
-	const settings = useSelect( ( select ) => select( CORE_STORE ).getSettings() );
+	const settings = useSelect( ( select ) =>
+		select( CORE_STORE ).getSettings()
+	);
 	const { updateSetting } = useDispatch( CORE_STORE );
 
 	return (
 		<>
-			<Button icon="admin-settings" className="wmd-header__actions--settings"
-				onClick={ () => setOpen( ! open ) }>
+			<Button
+				icon="admin-settings"
+				className="wmd-header__actions--settings"
+				onClick={ () => setOpen( ! open ) }
+			>
 				{ __( 'Settings', 'wp-mail-debugger' ) }
 			</Button>
 			{ open && (
-				<Popover position="bottom" headerTitle={ __( 'Edit Settings', 'wp-mail-debugger' ) } expandOnMobile
-					onClickOutside={ () => setOpen( false ) } onClose={ () => setOpen( false ) }>
+				<Popover
+					placement="bottom-start"
+					headerTitle={ __( 'Edit Settings', 'wp-mail-debugger' ) }
+					expandOnMobile
+					onClose={ () => setOpen( false ) }
+				>
 					<div className="wmd-settings">
 						<RadioControl
 							label={ __( 'Capture Mode', 'wp-mail-debugger' ) }
 							options={ [
-								{ label: __( 'Filter wp_mail()', 'wp-mail-debugger' ), value: 'filter' },
-								{ label: __( 'Override wp_mail()', 'wp-mail-debugger' ), value: 'override' },
+								{
+									label: __(
+										'Filter wp_mail()',
+										'wp-mail-debugger'
+									),
+									value: 'filter',
+								},
+								{
+									label: __(
+										'Override wp_mail()',
+										'wp-mail-debugger'
+									),
+									value: 'override',
+								},
 							] }
 							selected={ settings.capture_mode }
-							onChange={ ( captureMode ) => updateSetting( 'capture_mode', captureMode ) }
-							help={ __( 'How should WP Mail Debugger capture emails. By letting the original mail through and adding filters, or completely overriding wp_mail().', 'wp-mail-debugger' ) }
+							onChange={ ( captureMode ) =>
+								updateSetting( 'capture_mode', captureMode )
+							}
+							help={ __(
+								'How should WP Mail Debugger capture emails. By letting the original mail through and adding filters, or completely overriding wp_mail().',
+								'wp-mail-debugger'
+							) }
 						/>
 					</div>
 				</Popover>
